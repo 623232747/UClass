1e9b222ca62c1036cd1c096af090e3b6987943a0
PurpleFishKing <623232747@qq.com>
2021-02-20 02:38:18 +0800

    登录界面1.1 鉴权需改进 无分类

--- auth-server/src/main/java/org/BUPT/entity/User.java
+++ auth-server/src/main/java/org/BUPT/entity/User.java
@@ -13,7 +13,7 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Integer id;
+    private Long id;
 
     private String username;
 
@@ -23,11 +23,11 @@ public class User {
 
     private Date createTime;
 
-    public Integer getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(Integer id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
--- login-1.0/src/main/java/org/BUPT/controller/LoginController.java
+++ login-1.0/src/main/java/org/BUPT/controller/LoginController.java
@@ -1,5 +1,15 @@
 package org.BUPT.controller;
 //import org.BUPT.DAO.UserRepository;
+//import io.swagger.annotations.ApiImplicitParam;
+//import io.swagger.annotations.ApiImplicitParams;
+//import org.BUPT.authorization.annotation.Authorization;
+//import org.BUPT.authorization.annotation.CurrentUser;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import org.BUPT.DAO.UserRepository;
+import org.BUPT.authorization.Config.Constants;
+import org.BUPT.authorization.annotation.Authorization;
+import org.BUPT.authorization.annotation.CurrentUser;
 import org.BUPT.authorization.manager.TokenManager;
 import org.BUPT.authorization.model.TokenModel;
 import org.BUPT.entity.User;
@@ -10,32 +20,56 @@ import org.springframework.stereotype.Controller;
 import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @Controller
 @RequestMapping("/")
-public class LoginController{
+public class LoginController {
 
     @GetMapping("/")
-    public String index(){
+    public String index() {
         return "index";
     }
 
     @Autowired
     private TokenManager tokenManager;
+
+    @Autowired
+    private UserRepository userRepository;
+
     @PostMapping("/login")
     @ResponseBody
-    public String login(@RequestParam(value="username") String username,@RequestParam(value="password") String password){
+    public String login(@RequestParam(value = "userID") String userID, @RequestParam(value = "password") String password) {
 
-        Assert.notNull(username,"username can not be empty");
-        Assert.notNull(password,"password can not be empty");
-        User user = new User();
-        user.setUsername(username);
-        user.setPassword(password);
-        System.out.println(username+""+password);
-        user.setId(1212);
-//        TokenModel token = tokenManager.createToken(user.getId());
-//        System.out.println(token.getToken());
-        return "yes";
+        Assert.notNull(userID, "username can not be empty");
+        Assert.notNull(password, "password can not be empty");
+
+
+        User user = userRepository.findById(Long.parseLong(userID)).orElse(Constants.defaltUser);
+        if (user.getId() != null && user.getPassword().equals(password)) {
+            TokenModel token = tokenManager.createToken(user.getId());
+            System.out.println(token.getUserId());
+            System.out.println(tokenManager.checkToken(token));
+            return "LOGIN SECCESS";
+        } else {
+            return "LOGIN FAIL";
+        }
     }
+
+
+    @DeleteMapping ("/register")
+    @Authorization
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "authorization", value = "authorization", required = true, dataType = "string", paramType = "header"),
+    })
+    public ResponseEntity<String> logout(@CurrentUser User user) {
+//        System.out.println(user.getId());
+        tokenManager.deleteToken(user.getId());
+
+        return new ResponseEntity<>("LOGOUT", HttpStatus.OK);
+    }
+
+
 //
 //    @PostMapping("/register")
 //    public void register(){
